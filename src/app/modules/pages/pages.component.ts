a8299fc18b2cfde7c4295b0f7057fedb8a8d1538
wruben <wesleyruben213@hotmail.com>
2024-12-30 21:19:58 -0300

    adicionar icon e titulo

--- src/app/modules/pages/pages.component.ts
+++ src/app/modules/pages/pages.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { OrganizationDetail } from '../../core/models/organizacao.model';
 import { OrganizationDataService } from '../../service/data/organization-data.service';
 import { OrganizationService } from '../../service/organization.service';
+import { Title } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-pages',
@@ -15,11 +17,17 @@ export class PagesComponent {
   constructor(
     private organizationService: OrganizationService,
     private organizationDataService: OrganizationDataService,
-    private router: Router
+    private router: Router,
+    private titleService: Title,
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: object
   ) {}
 
   ngOnInit(): void {
     this.carregarDados();
+    if (isPlatformBrowser(this.platformId)) {
+      this.setPageTitleAndIcon();
+    }
   }
 
   carregarDados() {
@@ -34,4 +42,24 @@ export class PagesComponent {
   isHome(): boolean {
     return this.router.url === '/';
   }
+
+  setPageTitleAndIcon() {
+    // Definir o título
+    this.titleService.setTitle(this.organization.name);
+
+    // Definir o ícone (favicon)
+    const link: HTMLLinkElement = this.renderer.createElement('link');
+    link.rel = 'icon';
+    link.href = this.organization.logo.url;
+
+    // Remover favicon antigo, se existir
+    const oldLink = document.querySelector("link[rel='icon']");
+    if (oldLink) {
+      this.renderer.removeChild(document.head, oldLink);
+    }
+
+    // Adicionar novo favicon
+    this.renderer.appendChild(document.head, link);
+  }
+
 }
