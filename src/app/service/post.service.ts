e0090b8b11f9cf19a3f76c73118c05c59ce65708
wruben <wesleyruben213@hotmail.com>
2025-01-11 02:46:13 -0300

    teste 1

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -4,27 +4,30 @@ import { firstValueFrom, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Post } from '../core/models/post';
 
-const options =({
+const options = {
   headers: new HttpHeaders({
     'Cache-Control': 'no-cache',
-    'Pragma': 'no-cache'
-
-  })
-})
+    Pragma: 'no-cache',
+  }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
   private baseUrl = environment.api;
   private url = this.baseUrl + '/v1/instagram';
 
-
-
   constructor(private http: HttpClient) {}
 
   obterPosts(): Promise<Post[]> {
-    const options = {}; // Caso você precise de headers ou outros parâmetros
-    return firstValueFrom(this.http.get<Post[]>(`${this.url}/posts`));
+    const options = {
+      headers: {
+        'Cache-Control': 'no-cache',
+      }
+    }; // Caso você precise de headers ou outros parâmetros
+    
+    const timestamp = new Date().getTime();
+    return firstValueFrom(this.http.get<Post[]>(`${this.url}/posts?_=${timestamp}`, options));
   }
 }
